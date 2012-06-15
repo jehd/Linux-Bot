ae6a528cb595aea355fe4df650c07082422bfab1
Julian Essing <julian.essing@fh-heidelberg.de>
2012-06-15 06:11:40 +0200

    Datenbankfunktionen geupdatet, last seen hinzugefügt

--- dbase.c
+++ dbase.c
@@ -5,25 +5,46 @@
 
 #include "libircclient-1.6/include/libircclient.h"
 #include "sqlite3.h"
+#include <time.h>
 
-#define DB_FILE "test.db"
+
 
 sqlite3 *dbhandle;
 
 void add_db_entry(const char * event, const char * origin, const char ** params)
 {
-	sqlite3_exec(dbhandle, "INSERT INTO customer (firstname, lastname) VALUES (event, params)", NULL, NULL, NULL);
+	sqlite3_stmt *vm;
+        char* sql_query;
+	
+	time_t time = time(0);
+
+        sql_query = sqlite3_mprintf("INSERT INTO events (event, origin, params) VALUES ('%s', '%s', '%s', '%s')", event, origin, params, time);
+	sqlite3_finalize(vm);
+
 }
 
 void print_db()
 {
-	sqlite3_exec(dbhandle, "Select * FROM 'customer'", NULL, NULL, NULL);
+	sqlite3_exec(dbhandle, "Select * FROM 'events'", NULL, NULL, NULL);
+}
+
+char* last_seen(const char * username)
+{
+	sqlite3_stmt *vm;
+	char* sql_query;
+	sql_query = sqlite3_mprintf("Select time FROM 'event' WHERE origin = '%s' ORDER BY time DESC", username);
+	
+	sqlite3_prepare(handle, sql_query, strlen(sql_query), &vm, NULL);
+
+	sqlite3_finalize(vm);
+
+	return sql_query;
 }
 
 
 void create_table(sqlite3 *handle)
 {
 
-	sqlite3_exec(handle, "CREATE TABLE customer (id integer primary key, firstname text, lastname text);", NULL, NULL, NULL);
+	sqlite3_exec(handle, "CREATE TABLE events (id integer primary key, event text, origin text, params text, time date);", NULL, NULL, NULL);
 
 }
